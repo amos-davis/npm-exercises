6e69d3e4f2ce233150af52b931d9f17f4836c205
Amos Davis <amos.davis367@gmail.com>
2018-07-19 09:49:14 -0500

    trying to refactor hello message - xtra work

---
+++ src/say-hello.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const helloMessage = () => "It would be really cool if this actually works.";
+
+// let createButton = () => 'I guess I need to change this';
+
+module.exports = helloMessage;
